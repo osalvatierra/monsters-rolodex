80bcfa1595f2f2c44ca97b0f49040ab1511c4600
osalvatierra <oscar@owebdesigns.com>
2022-08-06 17:39:13 -0400

    testing v4

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
         )
       );
   }
-   //TESTING, TESTING AGAIN
+   //TESTING, TESTING AGAIN, testing again
   onSearchChange = (event) => {
     // console.log([...event.target.value]);
 

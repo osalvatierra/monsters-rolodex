7cc3165d502ebbae53d7f51792e13b541c24e54a
osalvatierra <oscar@owebdesigns.com>
2022-08-19 12:09:23 -0400

    functional component translation complete

--- src/App.js
+++ src/App.js
@@ -1,49 +1,45 @@
 import { useState, useEffect } from 'react';
 
-import CardList from "./components/card-list/card-list.component";
-import SearchBox from "./components/search-box/search-box.component";
-import "./App.css";
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+import './App.css';
 
 const App = () => {
-  const [searchField, setSearchField] = useState(''); // [value, setValue];
+  const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
   useEffect(() => {
-    console.log("Effect Fire")
-      fetch("https://jsonplaceholder.typicode.com/users")
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => setMonsters(users));
   }, []);
 
-  useEffect(()=> {
-      const newfilteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchField);
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
-    setFilterMonsters(newfilteredMonsters);
-  }, [monsters, searchField])
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
-    const searchFieldString = event.target.value.toLowerCase();
-    setSearchField(searchFieldString)
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
 
-
   return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
+    <div className='App'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
 
-        <SearchBox
-          className="monsters-search-box"
-          onChangeHandler={onSearchChange}
-          placeholder="search monsters"
-        />
-
-        <CardList monsters = {filteredMonsters} />
-      </div>
-    )
-}
-
+      <SearchBox
+        className='monsters-search-box'
+        onChangeHandler={onSearchChange}
+        placeholder='search monsters'
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,21 +1,18 @@
-import { Component } from "react";
+import './card.styles.css';
 
-import "./card.styles.css";
+const Card = ({ monster }) => {
+  const { id, name, email } = monster;
 
-class Card extends Component {
-	render() {
-		const { name, email, id } = this.props.monster;
-		return (
-			<div className="card-container" key={id}>
-				<img
-					alt={"monster ${name}"}
-					src={`https://robohash.org/${id}?set=set2&size=180x180`}
-				/>
-				<h2>{name}</h2>
-				<p>{email}</p>
-			</div>
-		);
-	}
-}
+  return (
+    <div className='card-container'>
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default Card;

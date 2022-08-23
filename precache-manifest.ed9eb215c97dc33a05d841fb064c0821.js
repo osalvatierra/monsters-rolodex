2c2f0ed3849a6df00439f72061b961eea37424d4
osalvatierra <oscar@owebdesigns.com>
2022-08-23 11:31:40 -0400

    Updates

---
+++ precache-manifest.ed9eb215c97dc33a05d841fb064c0821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eeb98bc21b849fdfaec2",
+    "url": "/monsters-rolodex/static/css/main.912ed499.chunk.css"
+  },
+  {
+    "revision": "eeb98bc21b849fdfaec2",
+    "url": "/monsters-rolodex/static/js/main.eeb98bc2.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/monsters-rolodex/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "fbfb3ec31794272bab82",
+    "url": "/monsters-rolodex/static/js/2.fbfb3ec3.chunk.js"
+  },
+  {
+    "revision": "baf071cd605113dee954",
+    "url": "/monsters-rolodex/static/js/runtime~main.baf071cd.js"
+  },
+  {
+    "revision": "5c9de5b453fa40e4202f9e50d7feb65d",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
